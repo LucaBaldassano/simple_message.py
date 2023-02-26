34b5493a32dc72123fb86bbec7f256d727bfcdc1
LucaBa <125908913+LucaBaldassano@users.noreply.github.com>
2023-02-26 18:24:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Display Message to the standard output
+print("System Failure Imminent")
+print("System Failure initeated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting outup motors")
+print("...rebooting hover engine")
+print()
+
+print("System online.")
